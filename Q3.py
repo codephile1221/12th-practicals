9179757e8ae711c58ea064428f4169e186e3fe6e
Lukshya2004 <56252087+Lukshya2004@users.noreply.github.com>
2020-10-27 09:32:37 +0400

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,16 @@
+#caesar cipher
+def caesar_cipher(word, shift):
+    new = []
+    for i in word:
+        if i != " ":
+            encrypted_char = ord(i) + shift
+            new.append(encrypted_char)
+    final = ""
+    for letter in new:
+        final += chr(letter)
+
+    return final
+
+choice = input()
+number = int(input())
+print(caesar_cipher(choice, number))
