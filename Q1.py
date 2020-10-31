1933bbd2541bb5a98d99a671466b665acb20912f
Lukshya2004 <56252087+Lukshya2004@users.noreply.github.com>
2020-11-01 02:39:25 +0400

    Add files via upload

--- Q1.py
+++ Q1.py
@@ -6,16 +6,16 @@ def piglatin(s):
     if s[n-1] == "y" and s[n-2] == "a":
         for i in s:
             l.append(i)
-
         l.pop()
         l.pop()
-
         for i in l:
             new += i
-
         final = l.pop()
         return(final + new[0:len(new)-1])
+    else:
+        return s
 
-word = input()
+word = input("please enter the word choice here: ")
 print(piglatin(word))
+
 
