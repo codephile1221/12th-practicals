b20473030a3ae3d02f1c9c37f55612aba8f99daa
Lukshya2004 <56252087+Lukshya2004@users.noreply.github.com>
2020-11-19 19:19:50 +0400

    Add files via upload

--- q13.py
+++ q13.py
@@ -1,17 +1,33 @@
-choice = input("PLease enter the string choice here: ")
-characters = [",", "?","!","."]
+def group_ascii(a):
+    sum = 0 
+    for i in a:
+        sum += ord(i)
+    return sum
+
+choice = input("please enter your string choice here : ")
+characters = [',','?','.','!']
 if choice[len(choice)-1] in characters:
     words = choice.split(" ")
-    a = words[len(words)-1]
-    final = ""
-    for i in range(len(a)):
-        if a[i] not in characters:
-            final += a[i]
+    last = words[len(words)-1]
+    temp_string = ""
+    for l in last:
+        if l.isalpha():
+            temp_string += l
     words.pop()
-    words.append(final)
-    words.sort()
-    for i in range(len(words)):
-        print(words[i], end = " ")
-else:
-    print("Enter the correct end of line character")
+    words.append(temp_string)
 
+    temp_list = []
+    for t in words:
+        a = group_ascii(t)
+        temp_list.append(a)
+    
+    temp_list.sort()
+    
+    for number in temp_list:
+        for word in words:
+            if group_ascii(word) == number:
+                print(word, end = " ")
+
+else:
+    print("invalid character, please enter the correct end of line character")
+    
